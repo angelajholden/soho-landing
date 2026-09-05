4a28f5772c3fe437bf1ab91682bc8eaab4b6e486
Angela J Holden <angelajholden@gmail.com>
2026-09-05 15:42:59 -0500

    finished styling homepage

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,3 +1,27 @@
 export function initInstagram(data) {
 	console.log(data);
+	const root = document.querySelector(".insta_footer-grid");
+	if (!root) return;
+
+	const instaGrid = document.querySelector(".insta_grid");
+
+	function renderInstaGrid(data, container) {
+		data.forEach((item) => {
+			const figure = document.createElement("figure");
+			figure.classList.add("figure");
+
+			const img = document.createElement("img");
+			img.src = item.media_url;
+			img.alt = "";
+
+			figure.append(img);
+			container.append(figure);
+		});
+	}
+
+	const slicedData = data.slice(0, 6);
+	renderInstaGrid(slicedData, root);
+
+	const sidebarData = data.slice(0, 9);
+	renderInstaGrid(sidebarData, instaGrid);
 }
