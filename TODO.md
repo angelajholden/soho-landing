02831999ed52b4b75358de461a1a8600b7eb3e5b
Angela J Holden <angelajholden@gmail.com>
2026-09-19 15:45:02 -0500

    styles and js for search and mobile nav

--- TODO.md
+++ TODO.md
@@ -1,13 +1,14 @@
 # @TODO
 
 - title tag and meta description
-- express server and instagram api link
+- express server and instagram api link DONE
 - open graph meta tags
-- image sourcing
+- image sourcing DONE
 
 ## Nav specific
 
-- noscript mobile menu
+- noscript mobile menu DONE
+- need skip nav
 - check 200% zoom
 - check 400% zoom
     - is mobile drawer fully a11y?
--- modules/navigation.js
+++ modules/navigation.js
@@ -1,18 +1,49 @@
 export default function initNavigation() {
 	const body = document.body;
-	const open = document.querySelector(".open_button");
-	const close = document.querySelector(".close_button");
-	const buttons = document.querySelectorAll(".menu_button");
-	const nav = document.querySelector(".navigation");
 
-	buttons.forEach((button) => {
-		button.addEventListener("click", () => {
-			const isActive = body.classList.toggle("menu_active");
-			if (isActive) {
-				open.setAttribute("aria-expanded", "true");
-			} else {
-				open.setAttribute("aria-expanded", "false");
-			}
-		});
-	});
+	// Navigation
+	const open = document.querySelector(".open_button");
+	const buttons = document.querySelectorAll(".menu_button");
+
+	// Search
+	const openSearch = document.querySelector(".open_search");
+	const searchButtons = document.querySelectorAll(".search_button");
+	const input = document.querySelector(".search_input");
+
+	function toggleMenuAndSearch(open, buttons, input, active) {
+		if (open) {
+			buttons.forEach((button) => {
+				button.addEventListener("click", () => {
+					const isActive = body.classList.toggle(active);
+					if (isActive) {
+						open.setAttribute("aria-expanded", "true");
+						if (input) {
+							setTimeout(() => {
+								input.focus();
+							}, 300);
+						}
+					} else {
+						open.setAttribute("aria-expanded", "false");
+						open.focus();
+					}
+				});
+			});
+		}
+	}
+	toggleMenuAndSearch(open, buttons, null, "menu_active");
+	toggleMenuAndSearch(openSearch, searchButtons, input, "search_active");
+
+	function escapeToClose(body, open, active) {
+		if (open) {
+			document.addEventListener("keydown", (e) => {
+				if (e.key === "Escape" && body.classList.contains(active)) {
+					body.classList.remove(active);
+					open.setAttribute("aria-expanded", "false");
+					open.focus();
+				}
+			});
+		}
+	}
+	escapeToClose(body, open, "menu_active");
+	escapeToClose(body, openSearch, "search_active");
 }
